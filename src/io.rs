3645d81ab73784a5068ab0ac062c6caccf23bc4f
Wael El Oraiby <wael.eloraiby@gmail.com>
2020-05-29 21:14:52 -0400

    Add error! and errorn!

--- src/io.rs
+++ src/io.rs
@@ -3,31 +3,47 @@ use core::*;
 #[link(name = "c")]
 extern "C"
 {
-	pub static mut stdin: *const libc::FILE;
+	pub static mut stdin: *mut libc::FILE;
 
-	pub static mut stdout: *const libc::FILE;
+	pub static mut stdout: *mut libc::FILE;
 
-    pub static mut stderr: *const libc::FILE;
+    pub static mut stderr: *mut libc::FILE;
 }
 
-pub fn printStrings<'a>(arr: &[&'a str]) {
+pub fn fprintStrings<'a>(f: *mut libc::FILE, arr: &[&'a str]) {
     for s in arr {
-        unsafe { libc::printf("%.*s\0".as_bytes().as_ptr() as *const i8, s.len(), s.as_bytes().as_ptr())};
+        unsafe { libc::fprintf(f,"%.*s\0".as_bytes().as_ptr() as *const i8, s.len(), s.as_bytes().as_ptr())};
     }
 }
 
 #[macro_export]
 macro_rules! printf {
     () => {{}};
-    ($($arg:tt),+) => {{
-       ::alt_std::io::printStrings(&[$($arg.toString().toStr(),)+])
+    ($($arg:expr),+) => {{
+       unsafe { ::alt_std::io::fprintStrings(::alt_std::io::stdout, &[$(($arg).toString().toStr(),)+]) }
     }};
 }
 
 #[macro_export]
 macro_rules! printfn {
     () => {{}};
-    ($($arg:tt),+) => {{
-       ::alt_std::io::printStrings(&[$($arg.toString().toStr(),)+ "\n"])
+    ($($arg:expr),+) => {{
+       unsafe { ::alt_std::io::fprintStrings(::alt_std::io::stdout, &[$(($arg).toString().toStr(),)+ "\n"]) }
+    }};
+}
+
+#[macro_export]
+macro_rules! error {
+    () => {{}};
+    ($($arg:expr),+) => {{
+       unsafe { ::alt_std::io::fprintStrings(::alt_std::io::stderr, &[$(($arg).toString().toStr(),)+]) }
+    }};
+}
+
+#[macro_export]
+macro_rules! errorn {
+    () => {{}};
+    ($($arg:expr),+) => {{
+       unsafe { ::alt_std::io::fprintStrings(::alt_std::io::stderr, &[$(($arg).toString().toStr(),)+ "\n"]) }
     }};
 }
