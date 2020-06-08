d49a72707a7ef433dd03a1f8ca3726e5327307ef
Wael El Oraiby <wael.eloraiby@gmail.com>
2020-06-07 22:40:54 -0400

    Add panic handler. Make containers public

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,22 @@
 #![allow(non_snake_case, non_camel_case_types)]
 #![no_std]
+
 pub mod ctypes;
 pub mod mem;
 pub mod io;
 pub mod vec;
 pub mod string;
 pub mod hashmap;
+
+pub use string::*;
+pub use vec::*;
+pub use hashmap::*;
+pub use ctypes::*;
+pub use mem::*;
+
+
+#[cfg(not(test))]
+#[panic_handler]
+fn alt_std_panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
